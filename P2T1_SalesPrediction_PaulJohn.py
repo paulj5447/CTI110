36d17869ece5d6ebb8041be83050743692db22eb
paulj5447 <55501135+paulj5447@users.noreply.github.com>
2019-09-18 11:19:39 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_PaulJohn.py
@@ -0,0 +1,16 @@
+#This program will calculate the profit of a company at 23%
+# 9/17/2019
+# CTI-110 P2T1 - Sales Prediction
+# John Paul
+
+# Get the projected sales
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23% of total sales.
+profit = total_sales * 0.23
+
+
+# Display the profit.
+print ('The profit is $', format(profit, ',.2f'))
+
+
